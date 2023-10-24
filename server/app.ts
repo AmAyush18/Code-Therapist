35888d6b5661338c42a6b804d72bd19311e681d8
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-25 00:57:43 +0530

    Register User API route implemented

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from 'cookie-parser';
 import { ErrorMiddleware } from './middleware/error';
+import userRouter from './routes/user.route';
 
 //body parser
 app.use(express.json({limit: "50mb"}));
@@ -15,6 +16,9 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.ORIGIN
 }));
+
+// routes
+app.use("/api/v1", userRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
