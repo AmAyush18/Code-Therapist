c32b5f931fbba5e7272d4222ca84a268124f31ce
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-24 18:02:51 +0530

    Implemented ErrorHandler Malwares

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,9 @@
+require('dotenv').config();
 import express, {NextFunction, Request, Response} from 'express';
 export const app = express();
 import cors from "cors";
 import cookieParser from 'cookie-parser';
-require('dotenv').config();
+import { ErrorMiddleware } from './middleware/error';
 
 //body parser
 app.use(express.json({limit: "50mb"}));
@@ -28,4 +29,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     const err = new Error(`Can't find ${req.originalUrl} on this server!`) as any;
     err.statusCode = 400;
     next(err);
-})
+});
+
+app.use(ErrorMiddleware);
--- server/server.ts
+++ server/server.ts
@@ -5,4 +5,4 @@ require("dotenv").config();
 app.listen(process.env.PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
     connectDB();
-})
+});
