f80c53e86b4a117f756e54bf1ab27439b1b73bc6
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-25 12:31:57 +0530

    Implemented Activate User Account through OTP

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,7 +1,7 @@
 import mongoose, {Document, Model, Schema} from "mongoose";
 import bcrypt from 'bcryptjs';
 
-const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+s/;
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
 
 export interface IUser extends Document{
     name: string;
