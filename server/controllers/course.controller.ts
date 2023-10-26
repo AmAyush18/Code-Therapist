46a8978405727db2cb4864488d23ffea6e6e55d0
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-26 15:45:13 +0530

    Implemented get all courses api route

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -73,4 +73,18 @@ export const getSingleCourse = CatchAsyncError(async(req: Request, res: Response
     } catch(error: any){
         return next(new ErrorHandler(error.message, 500));
     }
+});
+
+// get all courses -- without purchasing
+export const getAllCourses = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=>{
+    try{
+        const courses = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+        
+        res.status(200).json({
+            success: true,
+            courses,
+        });
+    } catch(error: any){
+        return next(new ErrorHandler(error.message, 400));
+    }
 })
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
 
 const courseRouter = express.Router();
@@ -21,6 +21,11 @@ courseRouter.put(
 courseRouter.get(
     "/get-course/:id", 
     getSingleCourse
+);
+
+courseRouter.get(
+    "/get-courses", 
+    getAllCourses
 );
 
 export default courseRouter;
