815639dacb6c6bb08887c0990d5c7d56fdbe01fc
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-27 15:18:15 +0530

    Implemented update notification status api route

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -17,3 +17,28 @@ export const getNotifications = CatchAsyncError(async(req:Request, res: Response
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+// update notification status --only admin
+export const updateNotification = CatchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+    try{
+        const notification = await NotificationModel.findById(req.params.id);
+        
+        if(!notification){
+            return next(new ErrorHandler("Notification not found", 404));
+        } else {
+            notification.status ? notification.status = 'read' : notification.status;
+        }
+        
+        await notification.save();
+   
+        const notifications = await NotificationModel.find().sort({createdAt: -1});
+        
+        res.status(200).json({
+            success: true,
+            notifications,
+        });
+
+    } catch (error: any){
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotification } from '../controllers/notification.controller';
 const notificationRouter = express.Router();
 
-notificationRouter.get("/get-all-notifications", isAuthenticated, authorizeRoles("admin"), getNotifications);
+notificationRouter.get(
+    "/get-all-notifications", 
+    isAuthenticated, 
+    authorizeRoles("admin"), 
+    getNotifications
+);
+
+notificationRouter.put(
+    "/update-notification/:id", 
+    isAuthenticated, 
+    authorizeRoles("admin"), 
+    updateNotification
+);
 
 export default notificationRouter;
